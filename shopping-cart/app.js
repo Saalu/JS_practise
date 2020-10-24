054a3d43c386e891fd31a9b2e2254a9c73eb0d51
unknown <saaluissaka>
2020-10-24 08:06:46 +0000

    get course from DOM functionality

--- shopping-cart/app.js
+++ shopping-cart/app.js
@@ -6,7 +6,47 @@ const cartDrop = document.querySelector('.cart-drop')
 eventListeners()
 function eventListeners(){
     document.addEventListener('click', dropCart)
+    document.addEventListener('click', buyCourse)
 }
+
+// ======== Functions =========
+
+function buyCourse(e){
+    e.preventDefault()
+console.log('course Added')
+    if(e.target.classList.contains('add-to-cart')){
+        const course = e.target.parentElement.parentElement
+        getCourse(course)
+    }
+
+}
+
+function getCourse(course){
+
+    const courseInfo = {
+        image:course.querySelector('img').src ,
+        title:course.querySelector('h4').textContent ,
+        price: course.querySelector('.price span').textContent,
+        id:course.querySelector('a').getAttribute('data-id') ,
+    }
+
+    console.log(courseInfo)
+
+    addIntoCart(courseInfo)
+}
+
+
+function addIntoCart(course){
+    
+}
+
+
+
+
+
+
+
+
 
 
 function dropCart(e){
